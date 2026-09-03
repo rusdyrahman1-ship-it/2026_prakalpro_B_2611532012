82ca36eacc14034075e6ab5d1f7a8de210819234
rusdyrahman1-ship-it <rusdyrahman1@gmail.com>
2026-09-03 12:34:39 +0700

    first commit

---
+++ Pekan 1/program 1_2611532012.py
@@ -0,0 +1,13 @@
+# Program pertama
+"""
+Komentar
+dua baris atau lebih
+pertemuan 1 pratikum alpro
+"""
+print ("Hello World")
+print()
+print("program ini menghasilkan")
+print("Empat baris output")
+print("/ \\ // \\\\ /// \\\\\\")
+print("program ini" + "dibuat oleh a\n"
+    +  "Muhammad Rusdy Rahman dan 2611532012")
