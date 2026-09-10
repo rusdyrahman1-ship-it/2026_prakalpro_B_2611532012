fe1fceac1c9f6db15fe25b41e143685e854dbd21
Skyyrainn <rusdyrahman1@gmail.com>
2026-09-10 09:54:09 +0700

    Update Konstanta_2611532012.py

--- Pekan2/Konstanta_2611532012.py
+++ Pekan2/Konstanta_2611532012.py
@@ -3,7 +3,7 @@
 # nama variabel ditambah 4 digit nim terakhir contoh: jari_1234
 
 from typing import final 
-PI_2012: Final= 3.14
+PI_2012: final = 3.14
 print("pi: %f" % (PI_2012))
 jari_2012 = float(input('Masukkan nilai jari-jari: '))
 luas_2012 = PI_2012 * jari_2012
