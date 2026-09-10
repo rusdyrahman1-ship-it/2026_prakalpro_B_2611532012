ab004d0c02b36363e72e09c9f77fc29dc2e45ddc
rusdyrahman1-ship-ite <rusdyrahman1@gmail.com>
2026-09-10 08:39:19 +0700

    pekann 2

--- Pekan 2/numeric_2611532012.py
+++ Pekan 2/numeric_2611532012.py
@@ -1,1 +1,13 @@
 # Buat file dengan nama Numerik_2012.py
+# nama variabel ditambah 4 digit terakhir NIM contoh:a_5500
+#create a variable with numeric type value.
+a_2012 = 100
+print("The type of variable having value", a_2012, "is", type(a_2012))
+
+#create a variable with float type value.
+b_2012 = 10.2345
+print("The type of variable having value", b_2012, "is", type (b_2012))
+
+#create a variale with complex type value.
+c_2012 = 10 + 3j
+print("The type of variable having value", c_2012, "is", type(c_2012))
