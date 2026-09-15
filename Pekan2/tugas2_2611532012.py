de0f0c4c6add563a72e239356d0c19de2bd2fa27
rusdyrahman1-ship-ite <rusdyrahman1@gmail.com>
2026-09-15 16:40:44 +0700

    Perbarui Pekan2

---
+++ Pekan2/tugas2_2611532012.py
@@ -0,0 +1,29 @@
+print("=== SISTEM REGISTRASI PRATIKAN ALPRO 2026 === ")
+nama_mahasiswa_2012 = "Muhammad Rusdy Rahman"
+jenis_kelamin_2012 = "L"
+umur_2012 = 18
+skor_tes_awal_2012 = 83.5
+alamat_2012 ="""
+Batu Belah
+Kec. Kampar
+Kab. Kampar
+Prov. Riau """
+ID_Token_Sinyal_2012 = 110 + 5j 
+batas_minimum_nilai_2012 = 75
+status_kelulusan_2012 = skor_tes_awal_2012 >= batas_minimum_nilai_2012
+print("Masukkan Nama Mahasiswa : ", nama_mahasiswa_2012)
+print("Masukkan Jenis Kelamin : ", jenis_kelamin_2012)
+print("Masukkan Umur : ", umur_2012)
+print("Masukkan Skor Tes awal : ", skor_tes_awal_2012)
+
+print("=== DATA PRATIKAN & HASIL PEMERIKSAAN ===")
+print("Nama Mahasiswa : ", nama_mahasiswa_2012 , "|" , "Tipe :", type(nama_mahasiswa_2012))
+print("Jenis Kelamin : ", jenis_kelamin_2012, "|" , "Tipe :", type(jenis_kelamin_2012))
+print("Alamat Domisili :", alamat_2012,"|", "Tipe :", type(alamat_2012))
+print("Umur :", umur_2012,"|", "Tipe :", type(umur_2012))
+print("Skor Tes Awal :", skor_tes_awal_2012, "|", "Tipe :", type(skor_tes_awal_2012))
+print("ID Token Sinyal :", ID_Token_Sinyal_2012, "|" ,"Tipe :", type(ID_Token_Sinyal_2012))
+
+print("=== STATUS KELULUSAN PRATIKUM ===")
+print("Batas Minimum Nilai :", batas_minimum_nilai_2012)
+print("Apakah Lulus?:", status_kelulusan_2012, "|", "Tipe: ", type(status_kelulusan_2012))
