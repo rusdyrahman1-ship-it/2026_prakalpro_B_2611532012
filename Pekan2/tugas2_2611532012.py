7bfb2e96cd5536ee4003ddc08c0af9147536b96b
rusdyrahman1-ship-ite <rusdyrahman1@gmail.com>
2026-09-15 21:28:35 +0700

    Pekan2

--- Pekan2/tugas2_2611532012.py
+++ Pekan2/tugas2_2611532012.py
@@ -2,13 +2,13 @@ print("=== SISTEM REGISTRASI PRATIKAN ALPRO 2026 === ")
 nama_mahasiswa_2012 = "Muhammad Rusdy Rahman"
 jenis_kelamin_2012 = "L"
 umur_2012 = 18
-skor_tes_awal_2012 = 83.5
+skor_tes_awal_2012 = 82.5
 alamat_2012 ="""
 Batu Belah
 Kec. Kampar
 Kab. Kampar
 Prov. Riau """
-id_token_sinyal_2012 = 110 + 5j 
+id_token_sinyal_2012 = 100 + 3j 
 batas_minimum_nilai_2012 = 75
 status_kelulusan_2012 = skor_tes_awal_2012 >= batas_minimum_nilai_2012
 print("Masukkan Nama Mahasiswa : ", nama_mahasiswa_2012)
