d2366537b89c8a6438a603307493f87aaa282c91
rusdyrahman1-ship-ite <rusdyrahman1@gmail.com>
2026-09-16 15:49:47 +0700

    Pekan2

--- Pekan2/tugas2_2611532012.py
+++ Pekan2/tugas2_2611532012.py
@@ -1,29 +1,25 @@
+from typing import final 
+batas_lulus_2012: final = 75.0
 print("=== SISTEM REGISTRASI PRATIKAN ALPRO 2026 === ")
-nama_mahasiswa_2012 = "Muhammad Rusdy Rahman"
-jenis_kelamin_2012 = "L"
-umur_2012 = 18
-skor_tes_awal_2012 = 82.5
-alamat_2012 ="""
-Batu Belah
-Kec. Kampar
-Kab. Kampar
-Prov. Riau """
-id_token_sinyal_2012 = 100 + 3j 
-batas_minimum_nilai_2012 = 75
-status_kelulusan_2012 = skor_tes_awal_2012 >= batas_minimum_nilai_2012
-print("Masukkan Nama Mahasiswa : ", nama_mahasiswa_2012)
-print("Masukkan Jenis Kelamin : ", jenis_kelamin_2012)
-print("Masukkan Umur : ", umur_2012)
-print("Masukkan Skor Tes awal : ", skor_tes_awal_2012)
-
+nama_mahasiswa_2012 = input("Masukkan Nama :")
+jenis_kelamin_2012 = input("Masukkan Jenis Kelamin (L/P) : ")
+umur_2012 = int(input("Masukkan Umur : "))
+skor_tes_awal_2012 =float(input("Masukkan Skor Tes Awal : "))
+print( " " )
 print("=== DATA PRATIKAN & HASIL PEMERIKSAAN ===")
+alamat_2012 ="""
+Batu Belah,
+Kec. Kampar,
+Kab. Kampar,
+Prov. Riau """
 print("Nama Mahasiswa : ", nama_mahasiswa_2012 , "|" , "Tipe :", type(nama_mahasiswa_2012))
 print("Jenis Kelamin : ", jenis_kelamin_2012, "|" , "Tipe :", type(jenis_kelamin_2012))
 print("Alamat Domisili :", alamat_2012,"|", "Tipe :", type(alamat_2012))
 print("Umur :", umur_2012,"|", "Tipe :", type(umur_2012))
 print("Skor Tes Awal :", skor_tes_awal_2012, "|", "Tipe :", type(skor_tes_awal_2012))
+id_token_sinyal_2012 =100+3j
 print("ID Token Sinyal :", id_token_sinyal_2012, "|" ,"Tipe :", type(id_token_sinyal_2012))
-
+print( " " )
 print("=== STATUS KELULUSAN PRATIKUM ===")
-print("Batas Minimum Nilai :", batas_minimum_nilai_2012)
-print("Apakah Lulus?:", status_kelulusan_2012, "|", "Tipe: ", type(status_kelulusan_2012))
+print("Batas Minimum Nilai :", batas_lulus_2012)
+print("Dinyatakan Lulus?:", skor_tes_awal_2012 >= batas_lulus_2012, "|", "Tipe: ", type(skor_tes_awal_2012 >= batas_lulus_2012))
